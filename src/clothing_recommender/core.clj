04985a74a84aa2f19c70e39fd4673ff6d1b8c18b
sara-slavkovic <saraslavkovic3@gmail.com>
2025-12-04 20:08:45 +0100

    Functions for simple clothes recommendation and filtering warm clothes

--- src/clothing_recommender/core.clj
+++ src/clothing_recommender/core.clj
@@ -1,6 +1,18 @@
 (ns clothing-recommender.core)
 
+(defn simple-recommendation
+      [temperature]
+      (if (< temperature 15)
+        "Wear a jacket."
+        "A t-shirt is fine."))
+
+(defn filter-warm-clothes
+      [items]
+      (filter #(= (:type %) :warm) items))
+
 (defn -main
-  "Entry function. Run with `clj -M:run` or from the REPL."
-  [& args]
-  (println "Working! This is your minimal Clojure project."))
+      [& _]
+      (println "Example 1:" (simple-recommendation 10))
+      (println "Example 2:" (filter-warm-clothes
+                              [{:name "Jacket" :type :warm}
+                               {:name "T-shirt" :type :light}])))
