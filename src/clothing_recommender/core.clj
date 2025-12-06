ed003c9a4e66ae58113b2ccd9fe813b84340f141
sara-slavkovic <saraslavkovic3@gmail.com>
2025-12-06 09:16:35 +0100

    Additional 5 functions for clothing recommendation

--- src/clothing_recommender/core.clj
+++ src/clothing_recommender/core.clj
@@ -1,18 +1,93 @@
 (ns clothing-recommender.core)
 
+;; clothing recommendation based on temperature
 (defn simple-recommendation
       [temperature]
       (if (< temperature 15)
         "Wear a jacket."
         "A t-shirt is fine."))
 
+;; filter warm clothes from items list
 (defn filter-warm-clothes
       [items]
       (filter #(= (:type %) :warm) items))
 
+;; filter products by brand, category and max price
+(defn filter-products
+  [products brand category max-price]
+  (filter (fn [product]
+            (and
+              (or (nil? brand) (= (:brand product) brand))
+              (or (nil? category) (= (:category product) category))
+              (or (nil? max-price) (<= (:price product) max-price))))
+          products))
+
+;; average rating by category
+(defn average-rating-by-category
+  [products category]
+  (let [filtered-products (filter #(= (:category %) category) products)]
+    (when (not (empty? filtered-products))
+      (let [ratings (map :rating filtered-products)
+            sum (reduce + ratings)
+            count (count ratings)]
+        (/ sum count)))))
+
+;; find most expensive product by color
+(defn most-expensive-by-color
+  [products color]
+  (let [filtered (filter #(= (:color %) color) products)]
+    (when (not (empty? filtered))
+      (reduce (fn [max-product current-product]
+                (if (> (:price current-product) (:price max-product))
+                  current-product
+                  max-product))
+              (first filtered)
+              (rest filtered)))))
+
+;; group products by brand
+(defn group-by-brand
+  [products]
+  (reduce (fn [acc product]
+            (let [brand (:brand product)
+                  existing-products (get acc brand [])]
+              (assoc acc brand (conj existing-products product))))
+          {}
+          products))
+
+;; recommend products by value
+(defn recommend-by-value
+  [products n]
+  (let [value-scores (map (fn [product]
+                            {:product product
+                             :score (/ (:rating product) (:price product))})
+                          products)
+        sorted (sort-by :score > value-scores)
+        top-n (take n sorted)]
+    (map :product top-n)))
+
 (defn -main
       [& _]
-      (println "Example 1:" (simple-recommendation 10))
-      (println "Example 2:" (filter-warm-clothes
-                              [{:name "Jacket" :type :warm}
-                               {:name "T-shirt" :type :light}])))
+      (let [products [{:product-id 1 :product-name "Blue Jeans" :brand "Levis" 
+                       :category "Pants" :price 89.99 :rating 4.5 :color "Blue" :size "M"}
+                      {:product-id 2 :product-name "White T-Shirt" :brand "Nike" 
+                       :category "Tops" :price 29.99 :rating 4.8 :color "White" :size "L"}
+                      {:product-id 3 :product-name "Black Jacket" :brand "Levis" 
+                       :category "Jackets" :price 120.00 :rating 4.2 :color "Black" :size "M"}
+                      {:product-id 4 :product-name "Red Dress" :brand "Zara" 
+                       :category "Dresses" :price 59.99 :rating 4.7 :color "Red" :size "S"}
+                      {:product-id 5 :product-name "Blue Jeans" :brand "Nike" 
+                       :category "Pants" :price 79.99 :rating 4.3 :color "Blue" :size "L"}]]
+        (println "Example 1:" (simple-recommendation 10))
+        (println "Example 2:" (filter-warm-clothes
+                                [{:name "Jacket" :type :warm}
+                                 {:name "T-shirt" :type :light}]))
+        (println "\nFilter Products:")
+        (println (filter-products products "Levis" nil 100))
+        (println "\nAverage Rating:")
+        (println (average-rating-by-category products "Pants"))
+        (println "\nMost Expensive by Color:")
+        (println (most-expensive-by-color products "Blue"))
+        (println "\nGroup by Brand:")
+        (println (group-by-brand products))
+        (println "\nRecommend by Value:")
+        (println (recommend-by-value products 3))))
--- test/clothing_recommender/core_test.clj
+++ test/clothing_recommender/core_test.clj
@@ -1,16 +1,85 @@
 (ns clothing-recommender.core-test
-    (:require [clothing-recommender.core :refer :all]
-      [clojure.test :refer :all]))
+  (:require [clothing-recommender.core :refer :all]
+            [clojure.test :refer :all]))
 
 (deftest simple-recommendation-test
-         (testing "simple clothing recommendation based on temperature"
-                  (is (= "Wear a jacket." (simple-recommendation 10)))
-                  (is (= "A t-shirt is fine." (simple-recommendation 20)))))
+  (testing "simple clothing recommendation based on temperature"
+    (is (= "Wear a jacket." (simple-recommendation 10)))
+    (is (= "A t-shirt is fine." (simple-recommendation 20)))))
 
 (deftest filter-warm-clothes-test
-         (testing "filter warm clothes from items list"
-                  (let [items [{:name "Jacket" :type :warm}
-                               {:name "T-shirt" :type :light}]]
-                       (is (= [{:name "Jacket" :type :warm}]
-                              (filter-warm-clothes items))))))
+  (testing "filter warm clothes from items list"
+    (let [items [{:name "Jacket" :type :warm}
+                 {:name "T-shirt" :type :light}]]
+      (is (= [{:name "Jacket" :type :warm}]
+             (filter-warm-clothes items))))))
 
+(def test-products
+  [{:product-id 1 :product-name "Blue Jeans" :brand "Levis"
+    :category   "Pants" :price 89.99 :rating 4.5 :color "Blue" :size "M"}
+   {:product-id 2 :product-name "White T-Shirt" :brand "Nike"
+    :category   "Tops" :price 29.99 :rating 4.8 :color "White" :size "L"}
+   {:product-id 3 :product-name "Black Jacket" :brand "Levis"
+    :category   "Jackets" :price 120.00 :rating 4.2 :color "Black" :size "M"}
+   {:product-id 4 :product-name "Red Dress" :brand "Zara"
+    :category   "Dresses" :price 59.99 :rating 4.7 :color "Red" :size "S"}
+   {:product-id 5 :product-name "Blue Jeans" :brand "Nike"
+    :category   "Pants" :price 79.99 :rating 4.3 :color "Blue" :size "L"}])
+
+(deftest filter-products-test
+  (testing "filter by brand"
+    (let [result (filter-products test-products "Levis" nil nil)]
+      (is (= 2 (count result)))
+      (is (= "Levis" (:brand (first result))))))
+
+  (testing "filter by category"
+    (let [result (filter-products test-products nil "Pants" nil)]
+      (is (= 2 (count result)))
+      (is (= "Pants" (:category (first result))))))
+
+  (testing "filter by max price"
+    (let [result (filter-products test-products nil nil 80.0)]
+      (is (= 3 (count result)))
+      (is (<= (:price (first result)) 80.0))))
+
+  (testing "filter by brand, category and max price"
+    (let [result (filter-products test-products "Levis" nil 100.0)]
+      (is (= 1 (count result)))
+      (is (= "Blue Jeans" (:product-name (first result)))))))
+
+(deftest average-rating-by-category-test
+  (testing "average rating by category"
+    (let [result (average-rating-by-category test-products "Pants")]
+      (is (not (nil? result)))
+      (is (< 4.0 result))
+      (is (> 5.0 result))))
+
+  (testing "return nil for non-existent category"
+    (is (nil? (average-rating-by-category test-products "Shoes")))))
+
+(deftest most-expensive-by-color-test
+  (testing "find most expensive product by color"
+    (let [result (most-expensive-by-color test-products "Blue")]
+      (is (not (nil? result)))
+      (is (= "Blue" (:color result)))
+      (is (= 89.99 (:price result)))))
+
+  (testing "return nil for non-existent color"
+    (is (nil? (most-expensive-by-color test-products "Green")))))
+
+(deftest group-by-brand-test
+  (testing "group by brand"
+    (let [result (group-by-brand test-products)]
+      (is (contains? result "Levis"))
+      (is (contains? result "Nike"))
+      (is (contains? result "Zara"))
+      (is (= 2 (count (get result "Levis"))))
+      (is (= 2 (count (get result "Nike"))))
+      (is (= 1 (count (get result "Zara")))))))
+
+(deftest recommend-by-value-test
+  (testing "recommend products by value"
+    (let [result (recommend-by-value test-products 2)]
+      (is (= 2 (count result)))
+      (is (not (nil? (first result))))
+      (is (not (nil? (second result)))))))
