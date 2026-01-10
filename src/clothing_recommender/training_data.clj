d84e17a946bfce4898f9446ab61e164b6be16b45
sara-slavkovic <saraslavkovic3@gmail.com>
2026-01-11 00:53:19 +0100

    Modified feature vector function, no longer using normalized price and rating attributes for ML model

--- src/clothing_recommender/training_data.clj
+++ src/clothing_recommender/training_data.clj
@@ -1,31 +1,12 @@
 (ns clothing-recommender.training-data
-  (:require [clothing-recommender.decision-tree :as tree]
-            [clothing-recommender.discretization :as disc]))
-
-(defn label-from-score
-  "Discretizes score into class label (Quinlan-style)."
-  [score]
-  (if (>= score 70)
-    :recommend
-    :not-recommend))
-
-;(defn feature-vector
-;  "Features - rule-based scoring."
-;  [user product score]
-;  {:price        (:price-norm product)
-;   :rating       (:rating-norm product)
-;   :size-match   (if (> (tree/size-score user product) 0) 1 0)
-;   :category     (if (> (tree/category-score user product) 0) 1 0)
-;   :brand        (if (> (tree/brand-score user product) 0) 1 0)
-;   :color        (if (> (tree/color-score user product) 0) 1 0)
-;   :label        (label-from-score score)})
+  (:require [clothing-recommender.decision-tree :as tree]))
 
 (defn feature-vector
   "ML features – no rule-based scoring."
   [user product]
   (let [ptype (tree/product-size-type product)]
-    {:price        (:price-norm product)
-     :rating       (:rating-norm product)
+    {:price        (:price product)
+     :rating       (:rating product)
 
      ;; size depends on product type {:tops "M" :pants "S"}
      :size-match   (and ptype
@@ -47,23 +28,3 @@
                          (get-in user [:preferences :colors]))
                      :match
                      :no-match)}))
-
-;(defn build-training-data
-;  "Creates training set: normalized attributes + class"
-;  [user products]
-;  (map (fn [p]
-;         (let [score (tree/decision-tree-score user p)
-;               fv (feature-vector user p score)]
-;           (disc/discretize-instance fv)))
-;       products))
-
-(defn build-training-data
-  "Creates ML training set (features + label from baseline)."
-  [user products]
-  (map (fn [p]
-         (let [score (tree/decision-tree-score user p)
-               fv    (feature-vector user p)]
-           (-> fv
-               (assoc :label (label-from-score score))
-               disc/discretize-instance)))
-       products))
