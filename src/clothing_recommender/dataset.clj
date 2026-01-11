a66c5bddce422493be8e48c360ee8ed6c9075d62
sara-slavkovic <saraslavkovic3@gmail.com>
2026-01-11 16:15:11 +0100

    Generating dataset once and storing it in resources file

--- src/clothing_recommender/dataset.clj
+++ src/clothing_recommender/dataset.clj
@@ -1,6 +1,7 @@
 (ns clothing-recommender.dataset
   (:require
-    [clothing-recommender.training-data :as td]))
+    [clothing-recommender.training-data :as td]
+    [clojure.edn :as edn]))
 
 ;; label generator
 (defn simulate-label
@@ -28,3 +29,16 @@
     (for [u users
           p products]
       (user-product-instance u p))))
+
+;;persistence
+(defn save-dataset
+  "Saves dataset to disk as EDN."
+  [dataset path]
+  (spit path (pr-str dataset)))
+
+(defn load-dataset
+  "Loads dataset from EDN file."
+  [path]
+  (-> path
+      slurp
+      edn/read-string))
